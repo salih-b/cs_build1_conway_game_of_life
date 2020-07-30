e7fdb65317b9818c8355614014f0e0939366f4b4
salih <salihmorta17@gmail.com>
2020-07-30 17:32:00 -0400

    all functionality complete some more styling can be implementeted

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { ButtonToolbar } from 'react-bootstrap';
+import { ButtonToolbar, DropdownButton} from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 
 // 
 class Box extends React.Component {
@@ -57,10 +58,21 @@ class Grid extends React.Component {
 
 // 
 class Buttons extends React.Component {
-
-	// handleSelect = (evt) => {
-	// 	this.props.gridSize(evt);
-	// }
+// ##################
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+  }
+  handlePreset = (evt) => {
+    if (evt === "1"){
+      this.props.preset_1();
+    }
+    else if (evt === "2"){
+      this.props.preset_2();
+    }
+    else if (evt === "3"){
+      this.props.preset_3();
+    }
+	}
 
 	render() {
 		return (
@@ -82,17 +94,30 @@ class Buttons extends React.Component {
 					  Fast
 					</button>
 					<button className="btn btn-default" onClick={this.props.seed}>
-					  Seed
+					  Random Seed
 					</button>
-					{/* <DropdownButton
+    
+					<DropdownButton
 						title="Grid Size"
+            className="btn"
 						id="size-menu"
 						onSelect={this.handleSelect}
 					>
-						<MenuItem eventKey="1">20x10</MenuItem>
-						<MenuItem eventKey="2">50x30</MenuItem>
-						<MenuItem eventKey="3">70x50</MenuItem>
-					</DropdownButton> */}
+						<Dropdown.Item eventKey="1">20x10</Dropdown.Item>
+						<Dropdown.Item eventKey="2">50x30</Dropdown.Item>
+						<Dropdown.Item eventKey="3">70x50</Dropdown.Item>
+					</DropdownButton>
+          {/* second drop down  */}
+          <DropdownButton
+						title="Pre-Sets"
+						id="size-menu"
+            className="btn"
+						onSelect={this.handlePreset}
+					>
+						<Dropdown.Item eventKey="1">Blinker</Dropdown.Item>
+						<Dropdown.Item eventKey="2">Spaceship</Dropdown.Item>
+						<Dropdown.Item eventKey="3">Toad</Dropdown.Item>
+					</DropdownButton>
 				</ButtonToolbar>
 			</div>
 			)
@@ -121,13 +146,14 @@ class Main extends React.Component {
     this.setState({
       gridFull:gridCopy
     });
+    console.log(gridCopy);
   }
   seed = () => {
 		let gridCopy = arrayClone(this.state.gridFull);
 		for (let i = 0; i < this.rows; i++) {
 			for (let j = 0; j < this.cols; j++) {
 				if (Math.floor(Math.random() * 4) === 1) {
-					gridCopy[i][j] = true;
+          gridCopy[i][j] = true;
 				}
 			}
 		}
@@ -160,26 +186,88 @@ class Main extends React.Component {
 		this.setState({
 			gridFull: grid,
 			generation: 0
-		});
+    });
+    this.pauseButton();
 	}
 
-	// gridSize = (size) => {
-	// 	switch (size) {
-	// 		case "1":
-	// 			this.cols = 20;
-	// 			this.rows = 10;
-	// 		break;
-	// 		case "2":
-	// 			this.cols = 50;
-	// 			this.rows = 30;
-	// 		break;
-	// 		default:
-	// 			this.cols = 70;
-	// 			this.rows = 50;
-	// 	}
-	// 	this.clear();
+  //  ######################
+	gridSize = (size) => {
+		switch (size) {
+			case "1":
+				this.cols = 20;
+				this.rows = 10;
+			break;
+			case "2":
+				this.cols = 50;
+				this.rows = 30;
+			break;
+			default:
+				this.cols = 70;
+				this.rows = 50;
+		}
+		this.clear();
 
-	// }
+  }
+  
+  
+
+  preset_1 = () => {
+    this.clear();
+		let gridCopy = arrayClone(this.state.gridFull);
+    let midRowIndex = gridCopy.length/2
+    let midRow = gridCopy[midRowIndex];
+    let midCol = (midRow.length /2)
+    midRow[midCol] = true;
+    midRow[midCol-1] = true;
+    midRow[midCol+1] = true;
+		this.setState({
+			gridFull: gridCopy
+		});
+  }
+  
+  preset_2 = () => {
+    this.clear();
+		let gridCopy = arrayClone(this.state.gridFull);
+    let midRowIndex = gridCopy.length/2
+    let midRow = gridCopy[midRowIndex];
+    let midCol = (midRow.length /2)
+    for (let i = 0; i < 6; i++) {
+      midRow[midCol-i] = true;
+    }
+    for (let i = 1; i < 5; i++){
+      gridCopy[midRowIndex+1][midCol-i] = true;
+    }
+    for (let i = 2; i < 6; i++){
+      gridCopy[midRowIndex-1][midCol] = true;
+      gridCopy[midRowIndex-1][midCol+1] = true;
+      gridCopy[midRowIndex-1][midCol-i] = true;
+    }
+    for (let i = 0; i < 2; i++){
+      gridCopy[midRowIndex-2][midCol-i] = true;
+    }
+		this.setState({
+			gridFull: gridCopy
+		});
+  }
+  preset_3 = () => {
+    this.clear();
+    console.log("in preset 3")
+		let gridCopy = arrayClone(this.state.gridFull);
+    let midRowIndex = gridCopy.length/2
+    let midRow = gridCopy[midRowIndex];
+    let midCol = (midRow.length /2)
+    for (let i = 0; i < 3; i++) {
+      midRow[midCol-i] = true;
+      console.log("in for loop");
+    }
+    for (let i = 1; i < 4; i++){
+      gridCopy[midRowIndex+1][midCol-i] = true;
+    }
+		this.setState({
+			gridFull: gridCopy
+    });
+    console.log(gridCopy, this.state.gridFull);
+	}
 
 	play = () => {
 		let g = this.state.gridFull;
@@ -207,13 +295,14 @@ class Main extends React.Component {
 
 	}
 
-	componentDidMount() {
-		this.seed();
-		this.playButton();
-  }
+	// componentDidMount() {
+	// 	this.seed();
+	// 	this.playButton();
+  // }
   
   render() {
     return (
+      <div className="buildProject">
       <div className="gol">
         <h1> The Game Of Life</h1>
         <Buttons
@@ -223,15 +312,36 @@ class Main extends React.Component {
 					fast={this.fast}
 					clear={this.clear}
 					seed={this.seed}
+          preset_1={this.preset_1}
+          preset_2={this.preset_2}
+          preset_3={this.preset_3}
 					gridSize={this.gridSize}
 				/>
+        <h4>Generations: {this.state.generation}</h4>
         <Grid
 					gridFull={this.state.gridFull}
 					rows={this.rows}
 					cols={this.cols}
 					selectBox={this.selectBox}
 				/>
-        <h1>Generations: {this.state.generation}</h1>
+      </div>
+      <div className="gol_info">
+        <h2>Game Rules:</h2>
+        <ol>
+          <li className="gameInfo">Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+          <li className="gameInfo">Any live cell with two or three live neighbours lives on to the next generation.</li>
+          <li className="gameInfo">Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+          <li className="gameInfo">Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+        </ol>
+        <h2>About This Algorithm:</h2>
+        <p className="gameInfo"> Gamoe of Life is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+        It is a zero-player game, for the Algorithm two arrays are used: one to hold the current generation, and
+         one to calculate its successor. Often 0 and 1 represent dead and live cells, respectively. A
+          nested for loop considers each element of the current array in turn, counting the live neighbours
+           of each cell to decide whether the corresponding element of the successor array should be 0 or 1.
+          The successor array is displayed. For the next iteration, the arrays swap roles so that the 
+          successor array in the last iteration becomes the current array in the next iteration.</p>
+      </div>
       </div>
     );
   }
